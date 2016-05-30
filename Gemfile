99678aae71a79347b3f445620a56626c1b0d88fc
shepitko <ya.shepitko@gmail.com>
2016-05-30 21:46:11 +0300

    refactor

--- Gemfile
+++ Gemfile
@@ -1,2 +1,6 @@
 gem 'rack'
 gem 'codebreaker', :git => "git://github.com/shepitko/codebreaker.git", :branch => "sketch"
+
+gem 'rspec'
+gem 'capybara'
+gem 'launchy'
---
+++ rspec/features/index_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+feature 'index page' %q{Main page of game 'Codebreaker', where can set property need for the game} do
+  scenario 'username input data' do
+    visit('/')
+    fill_in 'user', with:'testuser'
+    click_on 'START GAME'
+    expect(page).to have_content('Game')
+  end
+
+  scenario 'username invalid input data' do
+    visit('/')
+    fill_in 'user', with:''
+    click_on 'START GAME'
+    expect(page).to have_content('')
+  end
+end
